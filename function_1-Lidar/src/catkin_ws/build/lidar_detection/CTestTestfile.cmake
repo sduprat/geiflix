291333e98f92d29d75de51671882049d6a0bd86b
RomainChlt <romain.choulot@gmail.com>
2021-12-03 16:37:17 +0000

    Deleted devel and build in function 1

--- function_1-Lidar/src/catkin_ws/build/lidar_detection/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/romainchlt/Documents/geiflix/function_1-Lidar/src/catkin_ws/src/lidar_detection
-# Build directory: /home/romainchlt/Documents/geiflix/function_1-Lidar/src/catkin_ws/build/lidar_detection
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- function_1-Lidar/src/catkin_ws/build/rslidar_sdk/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/romainchlt/Documents/geiflix/function_1-Lidar/src/catkin_ws/src/rslidar_sdk
-# Build directory: /home/romainchlt/Documents/geiflix/function_1-Lidar/src/catkin_ws/build/rslidar_sdk
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src/rs_driver")
