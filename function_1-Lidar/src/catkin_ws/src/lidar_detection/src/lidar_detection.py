d558ae92deecd3ec016343a5d75359d42368e2b8
RomainChlt <romain.choulot@gmail.com>
2021-12-03 16:23:06 +0000

    Added in Function1 the camera calibration and RSsimulator

--- function_1-Lidar/src/catkin_ws/src/lidar_detection/src/lidar_detection.py
+++ function_1-Lidar/src/catkin_ws/src/lidar_detection/src/lidar_detection.py
@@ -1,44 +1,36 @@
 #!/usr/bin/env python
 
 import rospy
-import time
-import threading
 from sensor_msgs.msg import PointCloud2
-from threading import Lock
-import os
 import sensor_msgs.point_cloud2 as pc2
 import math
 
-
 def callback(data):
-    """
-    for field in data.fields:
-        print(field.name)
-        
-    test = pc2.read_points(data, skip_nans=True, field_names=("x","y", "z"))
-    print(test[0])
-"""
-    for p in pc2.read_points(data, field_names = ("x", "y", "z"), skip_nans=True):
-        #print " x : %f  y: %f  z: %f" %(p[0],p[1],p[2])
-        if (math.sqrt(p[0]*p[0] + p[1]*p[1] + p[2]*p[2] < 3)):
-            print(math.tan(p[1]/p[2]))
-            print("Proche !!!!!")
+
+    for coordinates in pc2.read_points(data, field_names = ("x", "y", "z"), skip_nans=True):
+        x = coordinates[0]
+        y = coordinates[1]
+        z = coordinates[2]
+
+        if (math.sqrt(x*x + y*y + z*z < 0.2)):
+            if x > 0: angle = math.degrees(math.atan(y/x))
+            elif x < 0: angle = math.degrees(math.atan(y/x) + math.pi)
+            else : angle = math.degrees(math.pi/2)
+            print(f"Object detected at 20cm at angle {angle}")
+
 
 def main():
-
-
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
     rospy.init_node('LIDAR_detection', anonymous=False)
-
-    rospy.Subscriber("/rs_points", PointCloud2, callback)
-
+    rospy.Subscriber("/rslidar_points", PointCloud2, callback)
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
     main()
+
 
