6c50293fcfa808305b47f3ea9730da163531f3a5
Neluji <thegeneral@pi.ka>
2021-12-09 12:06:31 +0100

    Begin of PID implementation

--- function_0-Raspberry/scripts/server.py
+++ function_0-Raspberry/scripts/server.py
@@ -6,11 +6,12 @@ import os
 import struct
 import socket
 from math import tan,pi
+from simple_pid import PID
 
 HOST = ''     # IP address on LAN
 PORT = 6666              # Arbitrary non-privileged port
 
-CMC = 0x010
+MCM = 0x010
 SSC = 0x020
 MS = 0x100
 US1 = 0x000
@@ -22,11 +23,9 @@ MAX_SPEED_FW = 75
 MAX_SPEED_BW = 25
 SPEED_STOP = 50
 
-<<<<<<< Updated upstream
 CAR_LENGTH = 60
 CAR_WIDTH = 40
-=======
-CAR_LEN_MM = 800
+CAR_LEN_MM = 600
 
 TARGET_DIST_MM = 1500
 
@@ -34,8 +33,9 @@ MAX_ERROR_MM = 2000
 MIN_ERROR_MM = -2000
 
 CORRECT_GAIN = ((MAX_SPEED_FW - MAX_SPEED_BW)/(MAX_ERROR_MM - MIN_ERROR_MM))
-K_GAIN = 1.5
->>>>>>> Stashed changes
+PID_P = 1.5
+PID_I = 0.5
+PID_D = 0
 
 class EthReceiver(Thread):
     def __init__(self,connSock, canBus):
@@ -48,22 +48,17 @@ class EthReceiver(Thread):
         self.speed_left = 0
         self.speed_right = 0
         self.steering = 0
+        # PID object : P,I, and D gain, and setpoint
+        self.pid = PID(CORRECT_GAIN * PID_P, PID_I, PID_D)
 
     def run(self):
         self.distance = 0
         self.angle = 0
         self.enable_speed = 0
         self.speed_cmd = 0
-<<<<<<< Updated upstream
         self.speed_left = 0
         self.speed_right = 0
         self.steering = 0
-=======
-        self.direction = 0
-        # self.movement = 0
-        # self.turn = 0
-        # self.enable_steering = 0
->>>>>>> Stashed changes
         usAvG,usAvD,usArrC,usArrG,usArrD,usAvC = 0,0,0,0,0,0
         
         while True :
@@ -100,13 +95,16 @@ class EthReceiver(Thread):
             # Update speed cmd according to the distance ()
             self.distance -= CAR_LEN_MM
             self.enable_speed = True
-            erreur = self.distance - TARGET_DIST_MM
-            if (erreur < -MAX_ERROR_MM):
-                self.speed_cmd = MAX_SPEED_BM
-            elif (erreur > MAX_ERROR_MM):
-                self.speed_cmd = MAX_SPEED_FW
-            else:
-                self.speed_cmd = int(K_GAIN*CORRECT_GAIN*erreur) + 50
+            erreur = TARGET_DIST_MM - self.distance
+            # if (erreur < -MAX_ERROR_MM):
+            #     self.speed_cmd = MAX_SPEED_BM
+            # elif (erreur > MAX_ERROR_MM):
+            #     self.speed_cmd = MAX_SPEED_FW
+            # else:
+            #     # self.speed_cmd = int(K_GAIN*CORRECT_GAIN*erreur) + 50
+            self.speed_cmd = int(self.pid(erreur)) + 50
+            if (self.speed_cmd > MAX_SPEED_FW): self.speed_cmd = MAX_SPEED_FW
+            if (self.speed_cmd < MAX_SPEED_BW): self.speed_cmd = MAX_SPEED_BW
 
             print("d>",self.distance,"/ a>",self.angle,"/ s>",self.speed_cmd)
 
@@ -117,8 +115,8 @@ class EthReceiver(Thread):
                 self.speed_cmd &= ~(1 << 7)
 
             # Compute differential speed for motors
-            self.speed_left = ((CAR_LENGTH + CAR_WIDTH * tan(2*pi*(self.angle/360)))/CAR_LENGTH) * self.speed_cmd + 50
-            self.speed_right = ((CAR_LENGTH - CAR_WIDTH * tan(2*pi*(self.angle/360)))/CAR_LENGTH) * self.speed_cmd + 50
+            # self.speed_left = int(((CAR_LENGTH + CAR_WIDTH * tan(2*pi*(self.angle/360)))/CAR_LENGTH) * self.speed_cmd) + 50
+            # self.speed_right = int(((CAR_LENGTH - CAR_WIDTH * tan(2*pi*(self.angle/360)))/CAR_LENGTH) * self.speed_cmd) + 50
 
             # Compute the steering from the angle
             if self.angle < -25:
@@ -129,7 +127,8 @@ class EthReceiver(Thread):
                 self.steering = 2*(self.angle+25) | (1 << 7)
 
             # Compose & send CAN message to Nucleo
-            toNucleo = can.Message(arbitration_id=SSC,data=[self.speed_left, self.speed_right,0,self.steering],extended_id=False)
+            # toNucleo = can.Message(arbitration_id=SSC,data=[self.speed_left, self.speed_right,0,self.steering],extended_id=False)
+            toNucleo = can.Message(arbitration_id=MCM,data=[self.speed_cmd, self.speed_cmd,0,self.steering],extended_id=False)
             self.canBus.send(toNucleo)
 
         print("Connexion perdue")
