3f01084eac3ee46c573273407d4bfd3159f00d6b
Tatger <corentin.tatger@outlook.fr>
2021-12-16 10:00:40 +0100

    Librairie pour ML

--- Machine_learning/Routine.py
+++ Machine_learning/Routine.py
@@ -31,22 +31,59 @@ Img_Per_Rot=2
 Curr_Image=0
 Predictions=[]
 
+def Img_Treatment(cam,Rot_time=10,Img_Per_Rot=2, Curr_Img=0,Model_Path = './cifar_net_V3.pth',S_dir="/home/corentin/Desktop/TSFlow",Disp=False) :
+
+    classes = ('Fire', 'No_Fire')
+
+    Get_data(cam,Display=Disp,index=Curr_Img)
+    os.chdir(S_dir)
+    testset=FireSet("ImageSet/",transform=transform)
+    testloader=torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+
+    # create a net object and load pretrained nn
+    net=Fire_Net()
+    net.load_state_dict(torch.load(Model_Path))
+
+    # get random training images with iter function
+    dataiter = iter(testloader)
+    images, labels = dataiter.next()
+
+    # run through net to make predictions
+    outputs = net(images)
+
+    _, predicted = torch.max(outputs, 1)
+    return predicted
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
-      
+    
     delay=0
     while Curr_Image < Img_Per_Rot:
         Turn_Start=time.time()
 
         if delay >= Rot_time/Img_Per_Rot:
-
-            Get_data(cap)
+            q=time.time()
+            Get_data(cap,Display=False,index=Curr_Image)
             os.chdir("/home/corentin/Desktop/TSFlow")
-
+            print("Photo prise en : ", time.time()-q)
             testset=FireSet("ImageSet/",transform=transform)
             testloader=torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
             # create classes tuple to display answers
             classes = ('Fire', 'No_Fire')
-
+            print("Dataset cree en : ",time.time()-q)
             # create a net object and load pretrained nn
             net=Fire_Net()
             net.load_state_dict(torch.load(Model_Path))
@@ -60,11 +97,13 @@ if __name__ == "__main__":
             outputs = net(images)
 
             #Collect net's answers and dislay them
+            
             _, predicted = torch.max(outputs, 1)
+            print(predicted)
             Predictions.append(classes[predicted])
             Curr_Image+=1
             delay=0
-
+            print("Traitement de l'image en : ", time.time()-q)
         Turn_End=time.time()
         delay+=(time.time()-Turn_Start)
 
